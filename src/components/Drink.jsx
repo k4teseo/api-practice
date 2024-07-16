15e675b25d1e96f30cbbba4e8b17f8029599e433
Kate Seo <kateseo@bu.edu>
2024-07-16 16:56:10 -0400

    added ingredients

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -23,18 +23,19 @@ const StyledImage = styled.img`
   margin-bottom: 5%;
 `;
 
+
 const StyledInfo = styled.h2`
   margin: 1%;
   font-size: calc(1px + 1vw);
 `;
 
-/*
+
 const StyledOL = styled.ol`
   text-align: left;
   margin-top: 0;
   padding-left: 0;
 `;
-*/
+
 
 const StyledP = styled.p`
 margin-top: 0;
@@ -42,12 +43,23 @@ width: 80%;
 text-align: center;
 `;
 
+
 function Drink(drink) {
     return (
         <StyledDiv>
             <StyledHeader>{drink.name}</StyledHeader>
             <StyledImage src={drink.image} alt={drink.alt}/>
             <StyledInfo>Glass: {drink.glass}</StyledInfo>
+            <StyledInfo>Ingredients:</StyledInfo>
+            <StyledOL>
+              {drink.ingredient1 && <li>{drink.measure1} {drink.ingredient1}</li>}
+              {drink.ingredient2 && <li>{drink.measure2} {drink.ingredient2}</li>}
+              {drink.ingredient3 && <li>{drink.measure3} {drink.ingredient3}</li>}
+              {drink.ingredient4 && <li>{drink.measure4} {drink.ingredient4}</li>}
+              {drink.ingredient5 && <li>{drink.measure5} {drink.ingredient5}</li>}
+              {drink.ingredient6 && <li>{drink.measure6} {drink.ingredient6}</li>}
+              {drink.ingredient7 && <li>{drink.measure7} {drink.ingredient7}</li>}
+            </StyledOL>
             <StyledInfo>Instructions:</StyledInfo>
             <StyledP>{drink.instructions}</StyledP>
         </StyledDiv>
